4094af48d9ef8bbe7726dc14e99e3002db92a791
raihanM95 <mahmud.raihan@yahoo.com>
2021-04-20 12:39:58 +0600

    user reg and login with jwt

--- src/main/java/com/msdsl/expensetracker/ExpenseTrackerApiApplication.java
+++ src/main/java/com/msdsl/expensetracker/ExpenseTrackerApiApplication.java
@@ -2,6 +2,11 @@ package com.msdsl.expensetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.msdsl.expensetracker.filters.AuthFilter;
+
 
 @SpringBootApplication
 public class ExpenseTrackerApiApplication {
